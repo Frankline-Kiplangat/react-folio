0426b54f234597d18af249d22708168b9631f422
Frankline-Kiplangat <kipfrankline@gmail.com>
2023-03-11 16:15:39 +0300

    add aboutme

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            
+            I am <span className="purple"> Frankline Kiplangat </span>
+            from <span className="purple"> Kenya.</span>
+            <br />I am a Fullstack Developer  and Technical Recruiter. I studied at The University of Nairobi and Moringa School.
+
+            <br />
+            eople find me to be an upbeat, self-motivated team player with excellent communication skills. 
+            I enjoy building everything from small business sites to portfolio themes and rich interactive web applications.
+            I enjoy the collaborative nature of working on a team building a product. 
+            I'm ready to jump onto a supportive team and begin contributing.
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Cooking
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Swimming
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Everything is possible, sacrifice for better tomorrow!"{" "}
+          </p>
+          <footer className="blockquote-footer">Frankline a.k.a Avocado</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
